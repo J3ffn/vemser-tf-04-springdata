68e69d2639685e424dc07a27b25ba93ef79d83d8
Livia <liviafausto03@gmail.com>
2023-08-12 11:08:22 -0300

    Removendo classe 'ConexaoBancoDeDados'

--- wbhealth/src/main/java/br/com/dbc/wbhealth/repository/AtendimentoRepository.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/repository/AtendimentoRepository.java
@@ -3,12 +3,9 @@ package br.com.dbc.wbhealth.repository;
 import br.com.dbc.wbhealth.exceptions.BancoDeDadosException;
 import br.com.dbc.wbhealth.exceptions.EntityNotFound;
 import br.com.dbc.wbhealth.model.entity.Atendimento;
-import br.com.dbc.wbhealth.model.enumarator.TipoDeAtendimento;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,233 +13,35 @@ public class AtendimentoRepository implements Repositorio<Integer, Atendimento> 
 
     @Override
     public Atendimento save(Atendimento atendimento) throws BancoDeDadosException {
-        Connection con = null;
-        try {
-            con = ConexaoBancoDeDados.getConnection();
-
-            Integer proximoAtendimentoId = this.getProximoId(con, "seq_atendimento.nextval");
-
-            atendimento.setIdAtendimento(proximoAtendimentoId);
-
-            String sqlAtendimento = """
-                    INSERT INTO Atendimento
-                    (id_atendimento, id_hospital, id_paciente, id_medico, data_atendimento, laudo, tipo_de_atendimento, valor_atendimento)
-                    VALUES(?, ?, ?, ?, ?, ?, ?, ?)
-                    """;
-
-            PreparedStatement stAtendimento = con.prepareStatement(sqlAtendimento);
-
-            stAtendimento.setInt(1, atendimento.getIdAtendimento());
-            stAtendimento.setInt(2, atendimento.getIdHospital());
-            stAtendimento.setInt(3, atendimento.getIdPaciente());
-            stAtendimento.setInt(4, atendimento.getIdMedico());
-            stAtendimento.setDate(5, Date.valueOf(atendimento.getDataAtendimento()));
-            stAtendimento.setString(6, atendimento.getLaudo());
-            stAtendimento.setString(7, atendimento.getTipoDeAtendimento().name());
-            stAtendimento.setDouble(8, atendimento.getValorDoAtendimento());
-
-            stAtendimento.executeUpdate();
-
-            return atendimento;
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(new Throwable("DEU RUIM NO SALVAMENTO!"));
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        return null;
     }
 
     private List<Atendimento> pupuleAtendimentos(Statement statement, String query) {
-        List<Atendimento> atendimentos = null;
-        try {
-            ResultSet res = statement.executeQuery(query);
-
-            atendimentos = new ArrayList<>();
-            while (res.next()) {
-                Integer idAtendimento = res.getInt("id_atendimento");
-                Integer idHospital = res.getInt("id_hospital");
-                Integer idPaciente = res.getInt("id_paciente");
-                Integer idMedico = res.getInt("id_medico");
-                LocalDate dataAtendimento = res.getDate("data_atendimento").toLocalDate();
-                String laudo = res.getString("laudo");
-                TipoDeAtendimento tipoDeAtendimento = TipoDeAtendimento.getTipo(res.getString("tipo_de_atendimento"));
-                Double valorAtendimento = res.getDouble("valor_atendimento");
-
-                Atendimento atendimento = new Atendimento(idAtendimento, idHospital, idPaciente, idMedico, dataAtendimento, laudo, tipoDeAtendimento, valorAtendimento);
-
-                atendimento.setIdAtendimento(idAtendimento);
-                atendimentos.add(atendimento);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return atendimentos;
+        return null;
     }
 
     @Override
     public List<Atendimento> findAll() throws BancoDeDadosException {
-        List<Atendimento> atendimentos;
-        Connection con = null;
-        try {
-            con = ConexaoBancoDeDados.getConnection();
-            Statement st = con.createStatement();
-
-            String sql = "SELECT * FROM ATENDIMENTO";
-
-            atendimentos = pupuleAtendimentos(st, sql);
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return atendimentos;
+        return null;
     }
 
     @Override
     public Atendimento findById(Integer id) throws BancoDeDadosException, EntityNotFound {
-        Atendimento atendimento;
-        Connection con = null;
-        try {
-            con = ConexaoBancoDeDados.getConnection();
-
-            String sql = "SELECT * FROM ATENDIMENTO "
-                    + "WHERE id_atendimento = " + id;
-
-            Statement st = con.prepareStatement(sql);
-
-            List<Atendimento> lista = this.pupuleAtendimentos(st, sql);
-
-            if (lista.isEmpty()) {
-                throw new EntityNotFound("Id inválido!");
-            }
-
-            atendimento = lista.get(0);
-
-            return atendimento;
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
+        return null;
     }
 
     @Override
     public Atendimento update(Integer id, Atendimento atendimento) throws BancoDeDadosException, EntityNotFound {
-        Connection con = null;
-        try {
-            Atendimento atendimentoBuscado = this.findById(id);
-
-            if (atendimentoBuscado == null) {
-                throw new EntityNotFound("ID de atendimento inválido!");
-            }
-
-            con = ConexaoBancoDeDados.getConnection();
-
-            StringBuilder sql = new StringBuilder();
-            sql.append("UPDATE ATENDIMENTO SET ");
-            sql.append("data_atendimento = ?, ");
-            sql.append("laudo = ?, ");
-            sql.append("tipo_de_atendimento = ? ");
-            sql.append("WHERE id_atendimento = ?");
-
-            PreparedStatement st = con.prepareStatement(sql.toString());
-
-            int index = 1;
-            st.setDate(index++, Date.valueOf(atendimento.getDataAtendimento()));
-            st.setString(index++, atendimento.getLaudo());
-            st.setString(index++, atendimento.getTipoDeAtendimento().name());
-            st.setInt(index++, atendimentoBuscado.getIdAtendimento());
-
-            st.execute();
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        return atendimento;
+        return null;
     }
 
     @Override
     public boolean deleteById(Integer id) throws BancoDeDadosException, EntityNotFound {
-        Connection con = null;
-        try {
-            Atendimento atendimento = findById(id);
-
-            if (atendimento == null) {
-                throw new EntityNotFound("Atendimento não encontrado!");
-            }
-
-            con = ConexaoBancoDeDados.getConnection();
-
-            String sql = "DELETE FROM ATENDIMENTO " +
-                    "WHERE id_atendimento = ?";
-
-            PreparedStatement st = con.prepareStatement(sql);
-
-            st.setInt(1, id);
-
-            return st.execute();
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        return false;
     }
 
     @Override
     public Integer getProximoId(Connection connection, String nextSequence) throws SQLException {
-        String sql = "SELECT " + nextSequence + " mysequence from DUAL";
-        Statement stmt = connection.createStatement();
-        ResultSet res = stmt.executeQuery(sql);
-
-        if (res.next()) {
-            return res.getInt("mysequence");
-        }
-
         return null;
     }
-
 }
--- wbhealth/src/main/java/br/com/dbc/wbhealth/repository/PacienteRepository.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/repository/PacienteRepository.java
@@ -6,241 +6,39 @@ import br.com.dbc.wbhealth.model.entity.Paciente;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class PacienteRepository implements Repositorio<Integer, Paciente> {
     @Override
     public List<Paciente> findAll() throws BancoDeDadosException {
-        List<Paciente> listaPacientes = new ArrayList<>();
-        Connection conexao = null;
-
-        try {
-            conexao = ConexaoBancoDeDados.getConnection();
-            final String QUERY_SQL = "SELECT * FROM PESSOA\n"
-                    + "INNER JOIN PACIENTE\n"
-                    + "ON PESSOA.ID_PESSOA = PACIENTE.ID_PESSOA";
-            Statement statement = conexao.createStatement();
-            ResultSet resultSet = statement.executeQuery(QUERY_SQL);
-
-            while (resultSet.next()) {
-                Paciente paciente = getPacienteFromResultSet(resultSet);
-                listaPacientes.add(paciente);
-            }
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-        } finally {
-            fecharConexaoComBancoDeDados(conexao);
-        }
-
-        return listaPacientes;
+        return null;
     }
 
     @Override
     public Paciente findById(Integer idPaciente) throws BancoDeDadosException, EntityNotFound {
-        Paciente paciente = null;
-        Connection conexao = null;
-
-        try {
-            conexao = ConexaoBancoDeDados.getConnection();
-            final String QUERY_SQL = "SELECT * FROM PACIENTE\n"
-                    + "INNER JOIN PESSOA ON PACIENTE.ID_PACIENTE = ? "
-                    + "AND PESSOA.ID_PESSOA = PACIENTE.ID_PESSOA\n";
-            PreparedStatement statement = conexao.prepareStatement(QUERY_SQL);
-            statement.setInt(1, idPaciente);
-            statement.executeQuery();
-            ResultSet resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                paciente = getPacienteFromResultSet(resultSet);
-            }
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-        } finally {
-            fecharConexaoComBancoDeDados(conexao);
-        }
-
-        if (paciente == null)
-            throw new EntityNotFound("Paciente não encontrado!");
-
-        return paciente;
+        return null;
     }
 
     @Override
     public Paciente save(Paciente paciente) throws BancoDeDadosException {
-        Paciente novoPaciente;
-        Connection conexao = null;
-
-        try {
-            conexao = ConexaoBancoDeDados.getConnection();
-
-            final String QUERY_PESSOA = "INSERT INTO PESSOA\n"
-                    + "(id_pessoa, nome, cep, data_nascimento, cpf, salario_mensal, email)\n"
-                    + "VALUES(?, ?, ?, ?, ?, ?, ?)\n";
-            inserirNaTabelaPessoa(paciente, conexao, QUERY_PESSOA);
-
-            final String QUERY_PACIENTE = "INSERT INTO PACIENTE\n"
-                    + "(id_paciente, id_hospital, id_pessoa)\n"
-                    + "VALUES(?, ?, ?)\n";
-            inserirNaTabelaPaciente(paciente, conexao, QUERY_PACIENTE);
-
-            novoPaciente = findById(paciente.getIdPaciente());
-
-        } catch (SQLException | EntityNotFound e) {
-            throw new BancoDeDadosException(e.getCause());
-        } finally {
-            fecharConexaoComBancoDeDados(conexao);
-        }
-
-        return novoPaciente;
+        return null;
     }
 
     @Override
     public Paciente update(Integer idPaciente, Paciente pacienteModificado)
             throws BancoDeDadosException, EntityNotFound {
-        Paciente pacienteAtualizado = null;
-        Connection conexao = null;
-
-        try {
-            pacienteAtualizado = findById(idPaciente);
-            conexao = ConexaoBancoDeDados.getConnection();
-            final String UPDATE_QUERY = "UPDATE PESSOA SET \n"
-                    + "nome = ?, "
-                    + "cep = ?, "
-                    + "data_nascimento = ?, "
-                    + "cpf = ?, "
-                    + "salario_mensal = ?, "
-                    + "email = ?"
-                    + "WHERE id_pessoa = ?";
-
-            PreparedStatement preparedStatement = conexao.prepareStatement(UPDATE_QUERY);
-            preparedStatement.setString(1, pacienteModificado.getNome());
-            preparedStatement.setString(2, pacienteModificado.getCep());
-            preparedStatement.setDate(3, Date.valueOf(pacienteModificado.getDataNascimento()));
-            preparedStatement.setString(4, pacienteModificado.getCpf());
-            preparedStatement.setDouble(5, pacienteModificado.getSalarioMensal());
-            preparedStatement.setString(6, pacienteAtualizado.getEmail());
-            preparedStatement.setInt(7, pacienteAtualizado.getIdPessoa());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-        } finally {
-            fecharConexaoComBancoDeDados(conexao);
-        }
-
-        return pacienteAtualizado;
+        return null;
     }
 
     @Override
     public boolean deleteById(Integer idPaciente) throws BancoDeDadosException, EntityNotFound {
-        Connection conexao = null;
-        try {
-            Paciente deletarPaciente = findById(idPaciente);
-            conexao = ConexaoBancoDeDados.getConnection();
-
-            final String PACIENTE_QUERY = "DELETE FROM PACIENTE WHERE ID_PACIENTE = ?";
-            PreparedStatement stPaciente = conexao.prepareStatement(PACIENTE_QUERY);
-            stPaciente.setInt(1, idPaciente);
-            int resPaciente = stPaciente.executeUpdate();
-            if (resPaciente == 0)
-                return false;
-
-            final String PESSOA_QUERY = "DELETE FROM PESSOA WHERE ID_PESSOA = ?";
-            PreparedStatement stPessoa = conexao.prepareStatement(PESSOA_QUERY);
-            stPessoa.setInt(1, deletarPaciente.getIdPessoa());
-            int resPessoa = stPessoa.executeUpdate();
-
-            return resPessoa > 0;
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-        } finally {
-            fecharConexaoComBancoDeDados(conexao);
-        }
+        return false;
     }
 
     @Override
     public Integer getProximoId(Connection connection, String nextSequence) throws SQLException {
-        try {
-            String sql = "SELECT " + nextSequence + " mysequence from DUAL";
-            Statement stmt = connection.createStatement();
-            ResultSet res = stmt.executeQuery(sql);
-
-            if (res.next()) {
-                return res.getInt("mysequence");
-            }
-
-            return null;
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-        }
-    }
-
-    private Paciente getPacienteFromResultSet(ResultSet resultSet) throws SQLException {
-        Integer idPessoa = resultSet.getInt("id_pessoa");
-        String nome = resultSet.getString("nome");
-        String cep = resultSet.getString("cep");
-        LocalDate data = resultSet.getDate("data_nascimento").toLocalDate();
-        String cpf = resultSet.getString("cpf");
-        Double salarioMensal = resultSet.getDouble("salario_mensal");
-        String email = resultSet.getString("email");
-        Integer idPaciente = resultSet.getInt("id_paciente");
-        Integer idHospital = resultSet.getInt("id_hospital");
-
-        String dataFormatada = data.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-
-        Paciente paciente = new Paciente(nome, cep, dataFormatada, cpf,
-                salarioMensal, email, idHospital);
-
-        paciente.setEmail(email);
-
-        paciente.setIdPessoa(idPessoa);
-        paciente.setIdPaciente(idPaciente);
-
-        return paciente;
-    }
-
-    private void fecharConexaoComBancoDeDados(Connection conexao) throws BancoDeDadosException {
-        try {
-            if (conexao != null) {
-                conexao.close();
-            }
-        } catch (SQLException e) {
-            throw new BancoDeDadosException(e.getCause());
-        }
-    }
-
-    private void inserirNaTabelaPessoa(Paciente paciente, Connection conexao, String query) throws SQLException {
-        Integer idPessoa = this.getProximoId(conexao, "seq_pessoa.nextval");
-        paciente.setIdPessoa(idPessoa);
-
-        PreparedStatement stPesssoa = conexao.prepareStatement(query);
-        stPesssoa.setInt(1, paciente.getIdPessoa());
-        stPesssoa.setString(2, paciente.getNome());
-        stPesssoa.setString(3, paciente.getCep());
-        stPesssoa.setDate(4, Date.valueOf(paciente.getDataNascimento()));
-        stPesssoa.setString(5, paciente.getCpf());
-        stPesssoa.setDouble(6, paciente.getSalarioMensal());
-        stPesssoa.setString(7, paciente.getEmail());
-
-        stPesssoa.executeUpdate();
-    }
-
-    private void inserirNaTabelaPaciente(Paciente paciente, Connection conexao, String query) throws SQLException {
-        Integer proximoPacienteId = this.getProximoId(conexao, "seq_paciente.nextval");
-        paciente.setIdPaciente(proximoPacienteId);
-
-        PreparedStatement stPaciente = conexao.prepareStatement(query);
-        stPaciente.setInt(1, paciente.getIdPaciente());
-        stPaciente.setInt(2, 1);
-        stPaciente.setInt(3, paciente.getIdPessoa());
-
-        stPaciente.executeUpdate();
+        return null;
     }
 
 }
